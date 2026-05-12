eb5562c2f326a75f54b7e74fee0d1d7dbd0cff48
Keshav Maiya <keshavrajmaiya@gmail.com>
2026-05-13 00:09:40 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({
+        'message' : "hello from flask in docker",
+        'status': 'Live'
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({"status" : 'health'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
